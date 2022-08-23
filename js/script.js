5dafdb88c0560f139017f2ff2125678cba6b6e35
Cemalettin Durgun <cdurgun@gmail.com>
2022-08-23 17:16:48 +0300

    Export png function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+function exportAsPng() {
+    html2canvas(document.querySelector("body")).then(canvas => {
+        canvas.toBlob(function(blob) {
+            saveAs(blob, "git-cheat-sheet.png");
+            //let file = new Blob([blob], {type: "application/octet-stream"});
+            //let blobURL = URL.createObjectURL(file);
+            //window.location.href = blobURL;
+        })
+    });
+}
+
 document.querySelectorAll('div').forEach(item => {
     item.onmouseover = () => {
         item.style.color = "red"
